b29a33d828b8696a3151a5029bbd55378cdae681
Rofif007 <79833612+Rofif007@users.noreply.github.com>
2021-05-02 20:55:30 +0800

    20190140008_RofifAthoyaNurPrabu

---
+++ app/src/main/java/com/example/kelasasqlite/LihatData.java
@@ -0,0 +1,30 @@
+package com.example.kelasasqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.kelasasqlite.R;
+
+public class LihatData extends AppCompatActivity {
+    TextView tnama, tnomor;
+    String id, nm, tlp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data);
+
+        tnama = findViewById(R.id.tvNamaKontak);
+        tnomor = findViewById(R.id.tvNomorTelepon);
+
+        id = getIntent().getStringExtra("id");
+        nm = getIntent().getStringExtra("nama");
+        tlp = getIntent().getStringExtra("telpon");
+
+        setTitle("Lihat Data");
+        tnama.setText(nm);
+        tnomor.setText(tlp);
+    }
+}
--- app/src/main/java/com/example/kelasasqlite/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/kelasasqlite/adapter/TemanAdapter.java
@@ -1,22 +1,32 @@
 package com.example.kelasasqlite.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kelasasqlite.EditTeman;
+import com.example.kelasasqlite.LihatData;
+import com.example.kelasasqlite.MainActivity;
 import com.example.kelasasqlite.R;
+import com.example.kelasasqlite.database.DBController;
 import com.example.kelasasqlite.database.Teman;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
+    private Context context;
 
     public TemanAdapter(ArrayList<Teman> listData) {
         this.listData = listData;
@@ -31,15 +41,60 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
 
     @Override
     public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
-        String nm, tlp;
+        String nm, tlp, id;
 
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
+        id = listData.get(position).getId();
+        DBController controller = new DBController(context);
 
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setTextSize(20);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popup = new PopupMenu(context, holder.cardku);
+                popup.inflate(R.menu.popup_menu);
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.edit:
+                                Intent i = new Intent(context, EditTeman.class);
+                                i.putExtra("id", id);
+                                i.putExtra("nm", nm);
+                                i.putExtra("tlp", tlp);
+                                context.startActivity(i);
+                                break;
+                            case R.id.delete:
+                                HashMap<String, String> qvalues = new HashMap<>();
+                                qvalues.put("id", id);
+                                controller.deleteData(qvalues);
+                                Intent in = new Intent(context, MainActivity.class);
+                                context.startActivity(in);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popup.show();
+                return true;
+            }
+        });
+        holder.cardku.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, LihatData.class);
+                intent.putExtra("id", id);
+                intent.putExtra("nm", nm);
+                intent.putExtra("tlp", tlp);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
